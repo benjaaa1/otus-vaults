5932ed4bc5a82ed00f491ef70c92240a8465ebc9
Ben <ben@otus.finance>
2022-09-16 11:29:57 +0300

    add tailwind and next

--- packages/react-app/src/tailwind.config.js
+++ packages/react-app/src/tailwind.config.js
@@ -1,11 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {},
+    fontFamily: {
+      sans: ['"IBM Plex Sans"', 'sans-serif'],
+      serif: ['"IBM Plex Mono"', 'serif'],
+    },
+    colors: {
+      'black': '#1c1c1c',
+      'white': '#ffffff',
+      'gray': '#959595',
+      'dark-gray': '#343434',
+      'green': '#84FFC4'
+    },
   },
   plugins: [],
-}
+};
