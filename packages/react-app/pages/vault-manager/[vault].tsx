5859d2b0137d642a759e86fcda5ffb62e2f19fac
Benjamin Efigenio <befigenio@gmail.com>
2023-01-13 19:45:20 +0200

    display icon in cell

--- packages/react-app/pages/vault-manager/[vault].tsx
+++ packages/react-app/pages/vault-manager/[vault].tsx
@@ -2,12 +2,10 @@ import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
 import VaultManagement from '../../components/VaultManager/VaultManagement'
-import { getTwitterInfo, TwitterData } from '../api/utils/twitter'
+import { TwitterData } from '../api/utils/twitter'
 
-const VaultManager: NextPage<{ twitter: TwitterData }> = (props) => {
+const VaultManager: NextPage = () => {
 
-  // const { username, profile_image_url } = props.twitter.data;
-  // console.log(username, profile_image_url)
   return (
     <div className="flex h-full flex-col">
       <Head>
@@ -19,7 +17,6 @@ const VaultManager: NextPage<{ twitter: TwitterData }> = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <VaultManagement twitterHandle={username} twitterProfileImage={profile_image_url} /> */}
       <VaultManagement />
 
     </div>
