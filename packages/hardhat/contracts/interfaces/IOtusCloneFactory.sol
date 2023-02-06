76f45fa9f976328cc8c2f39844728f1b4efd0f06
Benjamin Efigenio <befigenio@gmail.com>
2023-02-06 10:21:56 +0200

    strategy address not set on init of vault

--- packages/hardhat/contracts/interfaces/IOtusCloneFactory.sol
+++ packages/hardhat/contracts/interfaces/IOtusCloneFactory.sol
@@ -1,30 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import {Vault} from '../libraries/Vault.sol';
-import {StrategyBase} from '../dov/strategy/StrategyBase.sol';
+import {Vault} from "../libraries/Vault.sol";
 
 interface IOtusCloneFactory {
   function cloneVault() external returns (address otusVaultClone);
 
-  function cloneStrategy() external returns (address strategyClone);
+  function cloneStrategy(uint _type) external returns (address strategyClone);
 
   function _initializeClonedVault(
     address _otusVaultClone,
     address _owner,
     Vault.VaultInformation memory _vaultInfo,
     Vault.VaultParams memory _vaultParams,
-    address _strategy,
     address _keeper
   ) external;
 
   /** v
    * @notice Clones strategy contract if supervisor has a vault created
    */
-  function _initializeClonedStrategy(
-    address _owner,
-    address _vault,
-    address _strategy,
-    StrategyBase.StrategyDetail memory _currentStrategy
-  ) external;
+  function _initializeClonedStrategy(address _owner, address _vault, address _strategy) external;
 }
