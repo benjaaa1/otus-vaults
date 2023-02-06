35a280db03b1098b2087246de2c10aea4254398b
Benjamin Efigenio <befigenio@gmail.com>
2023-02-06 10:19:49 +0200

    interface for strategy used by otus vault

--- packages/hardhat/contracts/interfaces/IStrategy.sol
+++ packages/hardhat/contracts/interfaces/IStrategy.sol
@@ -1,39 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import {Vault} from '../libraries/Vault.sol';
-import {StrategyBase} from '../dov/strategy/StrategyBase.sol';
-
 interface IStrategy {
-  function returnFundsAndClearStrikes() external;
+  function initialize(address _owner, address _vault) external;
 
-  // function setBoard(uint boardId) external;
+  function trade(address _strategy, bytes calldata data) external;
 
-  function trade(
-    StrategyBase.StrikeTrade memory currentStrikeStrategy
-  ) external returns (uint positionId, uint premiumReceived, uint capitalUsed, uint expiry, uint strikePrice);
+  function execute(bytes calldata data) external;
 
-  function reducePosition(bytes32 market, uint positionId, uint closeAmount) external;
+  function keeper(address _strategy, bytes calldata data) external;
 
-  function getVaultStrategy() external returns (StrategyBase.StrategyDetail memory currentStrategy);
-
-  function userHedge(bytes32 market, int size) external;
-
-  function dynamicDeltaHedge(bytes32 market, int deltaToHedge, uint deltaHedgeAttempts) external;
-
-  function _transferFunds(uint reservedHedgeFunds) external;
-
-  function _closeHedge() external;
-
-  function _closeHedgeEndOfRound() external;
-
-  function _transferToFuturesMarket(bytes32 market, int hedgeFunds) external;
-
-  function initialize(
-    address _owner,
-    address _vault,
-    StrategyBase.StrategyDetail memory _currentStrategy
-  ) external;
-
-  function _isLong(uint _optionType) external returns (bool isLong);
+  function close() external;
 }
