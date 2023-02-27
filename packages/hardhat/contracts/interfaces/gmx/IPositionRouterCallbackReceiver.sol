80496714d2dd2c8ab09fed3cad57694db5eb39dd
Benjamin Efigenio <befigenio@gmail.com>
2023-02-27 19:03:08 +0200

    not needed in optimism chain

--- packages/hardhat/contracts/interfaces/gmx/IPositionRouterCallbackReceiver.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: ISC
-
-pragma solidity 0.8.16;
-
-interface IPositionRouterCallbackReceiver {
-  function gmxPositionCallback(bytes32 positionKey, bool isExecuted, bool isIncrease) external;
-}
